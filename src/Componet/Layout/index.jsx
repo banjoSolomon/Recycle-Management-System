60f63a04a8e4268be8f65b1f02a77f3df395897b
banjoSolomon <ayomidebanjo68@gmail.com>
2024-07-28 13:45:01 +0100

    update

---
+++ src/Componet/Layout/index.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Footer";
+import {Outlet} from "react-router-dom";
+import Navbar from "../Navbar";
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+
+}
+
+export default Layout;
